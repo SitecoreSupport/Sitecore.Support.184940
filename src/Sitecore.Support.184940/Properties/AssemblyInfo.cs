d669b8da1021d4b8ddf0703320f78d1aeadaa74f
DK\dmsa <dmsa@sitecore.net>
2017-09-19 09:27:28 -0700

    Fix for issue #184940 - Installing the patch Sitecore.Support.416400 (7.0) breaks the "Send Email Message" action editor

---
+++ src/Sitecore.Support.184940/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.184940")]
+[assembly: AssemblyProduct("Sitecore.Support.184940")]
+[assembly: ComVisible(false)]
